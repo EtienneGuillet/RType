df901d702e3b3b4057b2578b975ec34c6fb689f6
Lilian ARAGO <lilian.arago@outlook.fr>
2019-11-09 01:41:53 +0100

    Added exit 1 if rfc generated is empty

--- docs/generate_rfc.sh
+++ docs/generate_rfc.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 RFC_NAME=rfc69420
 
+echo "Getting RFC as text format"
 curl -X POST https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F 'input=@./rfc.xml' -o $RFC_NAME
+echo "Getting RFC as HTML format"
 curl -X POST https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F 'input=@./rfc.xml' -F 'modeAsFormat=html/ascii' -o $RFC_NAME.html
+
+if [ -s rfc69420 ]
+then
+  exit 0
+else
+  exit 1
+fi
