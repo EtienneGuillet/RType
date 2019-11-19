e954ec52803e0f234dba97c9f86f76536f875c7c
Etienne Guillet <guilletetienne77@gmail.com>
2019-11-19 23:52:49 +0100

    add main with principal loop and basic world creation

--- client/sources/main.cpp
+++ client/sources/main.cpp
@@ -7,7 +7,28 @@
 
 /* Created the 07/11/2019 at 18:00 by julian.frabel@epitech.eu */
 
+#include "logger/DefaultLogger.hpp"
+
 int main()
 {
+    std::cout << "Loading default logger..." << std::endl;
+    b12software::logger::DefaultLogger::SetDefaultLogger(std::make_shared<b12software::logger::StandardLogger>());
+    std::cout << "Default logger loaded!" << std::endl;
+
+
+    std::unique_ptr<IECS> ecs = new ECS();
+    std::unique_ptr<IWorld> world = ecs.createWorld();
+
+
+    auto start = std::chrono::system_clock::now();
+    auto end = std::chrono::system_clock::now();
+    while () {
+        end = std::chrono_system_clock::now();
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() < 1000) {
+            start = std::chrono_system_clock::now();
+            world.tick();
+        }
+    }
+
     return 0;
 }
