e9479a3e423b107e0f6067ae128b71305ed6e8f9
Julian <julian.frabel@epitech.eu>
2019-11-19 15:47:22 +0100

    Added DLLoader

--- commons/sources/ecs/DLLoader/DLLoader.hpp
+++ commons/sources/ecs/DLLoader/DLLoader.hpp
@@ -2,6 +2,22 @@
 #define _DL_LOADER_HPP_
 
 #include <iostream>
+#include <memory>
+#include "DLLoaderException.hpp"
+
+#if defined(__linux__)
+    #include <dlfcn.h>
+    #ifndef LINUX
+        #define LINUX
+    #endif
+#elif defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
+    #include <windows.h>
+    #ifndef WINDOWS
+        #define WINDOWS
+    #endif
+#else
+    #error "This os distribution is not supported"
+#endif
 
 /*!
  * @namespace ecs
@@ -13,19 +29,69 @@ namespace ecs {
      * @class DLLoader
      * @brief A class that is used to load dynamic library, using path name.
      */
-    class DLLoader
-    {
+    class DLLoader {
+    private:
+#if defined(LINUX)
+        using HandlerType = void *;
+#else
+        using HandlerType = HINSTANCE;
+#endif
+
     public:
+        /*!
+         * @brief ctor
+         * @param libraryPath the path of the library to load
+         */
+        explicit DLLoader(const std::string &libraryPath)
+            : _instance()
+        {
+#if defined(LINUX)
+            _instance = dlopen(libraryPath.c_str(), RTLD_LAZY);
+            if (_instance == nullptr) {
+                throw DLLoaderException(dlerror(), WHERE);
+            }
+#else
+            _instance = LoadLibrary(TEXT(libraryPath.c_str()));
+            if (_instance == NULL) {
+                throw DLLoaderException("Failed to load '" + libraryPath + "'", WHERE);
+            }
+#endif
+        }
 
         /*!
-         * @brief Load a dynamic library.
+         * @brief dtor
          */
-        virtual void loadDL(const std::string &path) = 0;
+        ~DLLoader()
+        {
+#if defined(LINUX)
+            dlclose(_instance);
+#else
+            FreeLibrary(_instance);
+#endif
+        }
 
-        /*!
-         * @brief Unload a dynamic library.
-         */
-        virtual void unloadDL(const std::string &path) = 0;
+        template<typename API>
+        std::shared_ptr<API> loadAPI(const std::string &entryPoint) const
+        {
+#if defined(LINUX)
+            using EntryFunc = std::shared_ptr<API> (*)();
+            EntryFunc entry = reinterpret_cast<EntryFunc>(dlsym(_instance, entryPoint.c_str()));
+            if (entry == nullptr) {
+                throw DLLoaderException(dlerror(), WHERE);
+            }
+            return (*entry)();
+#else
+            using EntryFunc = std::shared_ptr<API> (__cdecl *)();
+            EntryFunc entry = reinterpret_cast<EntryFunc>(GetProcAddress(_instance, entryPoint.c_str()));
+            if (entry == NULL) {
+                throw DLLoaderException("Failed to find function '" + entryPoint + "'", WHERE);
+            }
+            return (entry)();
+#endif
+        }
+
+    private:
+        HandlerType _instance;
     };
 } /* ecs */
 
---
+++ commons/sources/ecs/DLLoader/DLLoaderException.hpp
@@ -0,0 +1,64 @@
+/*
+** EPITECH PROJECT, 2019
+** R-Type
+** File description:
+** DLLoaderException.hpp
+*/
+
+/* Created the 19/11/2019 at 15:33 by julian.frabel@epitech.eu */
+
+#ifndef R_TYPE_DLLOADEREXCEPTION_HPP
+#define R_TYPE_DLLOADEREXCEPTION_HPP
+
+#include <exception>
+#include <string>
+#ifndef WHERE
+    #define WHERE (std::string(__FILE__) + " line: " + std::to_string(__LINE__))
+#endif
+
+/*!
+ * @namespace ecs
+ * @brief Main namespace for all ecs related classes.
+ */
+namespace ecs {
+
+    /*!
+     * @class DLLoaderException
+     * @brief An exception throwed when the loader has a problem
+     */
+    class DLLoaderException : public std::exception {
+    public:
+        /*!
+         * @brief ctor
+         * @param msg A message describing the error
+         * @param where Information on where the error was throwed
+         */
+        DLLoaderException(const std::string &msg, const std::string &where)
+            : _what(msg), _where(where)
+        {};
+
+        /*!
+         * @brief Return the error message
+         * @return An error describing the message
+         */
+        const char *what() const noexcept override
+        {
+            return _what.c_str();
+        }
+
+        /*!
+         * @brief Return the error location
+         * @return A string describing the location
+         */
+        const char *where() const noexcept
+        {
+            return _where.c_str();
+        }
+
+    private:
+        std::string _what; /*!< The error message */
+        std::string _where; /*!< The error location */
+    };
+}
+
+#endif //R_TYPE_DLLOADEREXCEPTION_HPP
