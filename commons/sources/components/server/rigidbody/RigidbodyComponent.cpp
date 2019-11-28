eb8f35445635502eb696adef66c64d2196208efc
Julian <julian.frabel@epitech.eu>
2019-11-28 02:58:18 +0100

    Added rigidbody

---
+++ commons/sources/components/server/rigidbody/RigidbodyComponent.cpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2019
+** R-Type
+** File description:
+** RigidbodyComponent.cpp
+*/
+
+/* Created the 28/11/2019 at 02:50 by julian.frabel@epitech.eu */
+
+#include "RigidbodyComponent.hpp"
+
+ecs::components::RigidbodyComponent::RigidbodyComponent(float ups, const b12software::maths::Vector2D &direction)
+    : AComponent()
+    , _direction(direction)
+    , _ups(ups)
+{
+
+}
+
+const b12software::maths::Vector2D &ecs::components::RigidbodyComponent::getDirection() const
+{
+    return _direction;
+}
+
+void ecs::components::RigidbodyComponent::setDirection(const b12software::maths::Vector2D &direction)
+{
+    _direction = direction;
+}
+
+float ecs::components::RigidbodyComponent::getUps() const
+{
+    return _ups;
+}
+
+void ecs::components::RigidbodyComponent::setUps(float ups)
+{
+    _ups = ups;
+}
---
+++ commons/sources/components/server/rigidbody/RigidbodyComponent.hpp
@@ -0,0 +1,61 @@
+/*
+** EPITECH PROJECT, 2019
+** R-Type
+** File description:
+** RigidbodyComponent.hpp
+*/
+
+/* Created the 28/11/2019 at 02:50 by julian.frabel@epitech.eu */
+
+#ifndef R_TYPE_RIGIDBODYCOMPONENT_HPP
+#define R_TYPE_RIGIDBODYCOMPONENT_HPP
+
+#include <maths/Vectors.hpp>
+#include "ecs/IEntity/AComponent.hpp"
+#include "maths/Vectors.hpp"
+
+namespace ecs {
+
+    namespace components {
+
+        /*!
+         * @class RigidbodyComponent
+         * @brief A component used to compute physics
+         */
+        class RigidbodyComponent : public ecs::AComponent {
+        public:
+            RigidbodyComponent(float ups = 1, const b12software::maths::Vector2D &direction = b12software::maths::Vector2D());
+            ~RigidbodyComponent() = default;
+            RigidbodyComponent(const RigidbodyComponent &other) = default;
+            RigidbodyComponent &operator=(const RigidbodyComponent &other) = default;
+
+        public:
+            /*!
+             * @brief Get the direction of this component
+             * @return the direction of this component
+             */
+            const b12software::maths::Vector2D &getDirection() const;
+            /*!
+             * @brief Set the direction of this component
+             * @param direction The direction of this component
+             */
+            void setDirection(const b12software::maths::Vector2D &direction);
+            /*!
+             * @brief Get movement speed in unit per second
+             * @return the movement speed
+             */
+            float getUps() const;
+            /*!
+             * @brief Set the movement speed in unit per second
+             * @param ups the movement speed in unit per second
+             */
+            void setUps(float ups);
+
+        private:
+            b12software::maths::Vector2D _direction; /*!< The direction of this rb */
+            float _ups; /*!< movement unit per second */
+        };
+    }
+}
+
+#endif //R_TYPE_RIGIDBODYCOMPONENT_HPP
