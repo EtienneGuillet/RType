c34432c76c4b05b259dc04c85729894f5d6a09e0
Julian <julian.frabel@epitech.eu>
2019-11-28 02:49:41 +0100

    Added collider component

---
+++ commons/sources/components/server/collider/ColliderComponent.cpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2019
+** R-Type
+** File description:
+** ColliderComponent.cpp
+*/
+
+/* Created the 28/11/2019 at 02:42 by julian.frabel@epitech.eu */
+
+#include "ColliderComponent.hpp"
+
+ecs::components::ColliderComponent::ColliderComponent(const b12software::maths::Vector2D &size, const b12software::maths::Vector2D &offset)
+    : AComponent()
+    , _size(size)
+    , _offset(offset)
+{
+
+}
+
+const b12software::maths::Vector2D &ecs::components::ColliderComponent::getSize() const
+{
+    return _size;
+}
+
+void ecs::components::ColliderComponent::setSize(const b12software::maths::Vector2D &size)
+{
+    _size = size;
+}
+
+const b12software::maths::Vector2D &ecs::components::ColliderComponent::getOffset() const
+{
+    return _offset;
+}
+
+void ecs::components::ColliderComponent::setOffset(const b12software::maths::Vector2D &offset)
+{
+    _offset = offset;
+}
